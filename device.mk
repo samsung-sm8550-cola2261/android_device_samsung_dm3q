0a712c394a7d23aba79fbbea7de3d25b8fd20411
pjgowtham <pjgowtham@gmail.com>
2024-11-12 22:05:43 +0530

    dm3q: displayconfig: Setup density mapping
    Change-Id: I1a0cab5d554b072d889133128b49f8ccf4d4fb21

--- device.mk
+++ device.mk
@@ -12,6 +12,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_4630947093241269891.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947093241269891.xml
+
 # Init
 PRODUCT_PACKAGES += \
     init.dm3q.rc
