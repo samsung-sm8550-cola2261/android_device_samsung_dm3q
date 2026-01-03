0f1015ad53412a2d23c51194452cef24ca3c18a7
cola2261 <cola2261@proton.me>
2026-01-03 22:19:12 +0500

    dm3q: Set TARGET_HAS_UDFPS flag to true
    Change-Id: Ib13142d1eae06cc42034bb7ba953bb5a9ed07fb2

--- lineage_dm3q.mk
+++ lineage_dm3q.mk
@@ -14,6 +14,8 @@ $(call inherit-product, device/samsung/dm3q/device.mk)
 # Inherit from the Lineage configuration.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_HAS_UDFPS := true
+
 PRODUCT_NAME := lineage_dm3q
 PRODUCT_DEVICE := dm3q
 PRODUCT_BRAND := Samsung
