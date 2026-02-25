dd4ea97b6639599fb8957d4c9121dac6505fdaab
cola2261 <cola2261@proton.me>
2026-02-26 00:19:04 +0500

    dm3q: Update lineage_dm3q.mk
    Add PRODUCT_BUILD_PROP_OVERRIDES and replace upper case "Samsung" to stock "samsung"
    
    Change-Id: Ib3a41d9907b2308eda4c1c687c6d98655518ae43

--- lineage_dm3q.mk
+++ lineage_dm3q.mk
@@ -18,8 +18,14 @@ TARGET_HAS_UDFPS := true
 
 PRODUCT_NAME := lineage_dm3q
 PRODUCT_DEVICE := dm3q
-PRODUCT_BRAND := Samsung
+PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-S918B
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="dm3qxxx-user 15 AP3A.240905.015.A2 S918BXXS8DYI3 release-keys" \
+    BuildFingerprint=samsung/dm3qxxx/dm3q:15/AP3A.240905.015.A2/S918BXXS8DYI3:user/release-keys \
+    DeviceProduct=dm3q \
+    SystemName=dm3qxxx
