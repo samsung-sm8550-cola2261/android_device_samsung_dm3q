e26dd0d6b723554117f83bcb2a352e193f3a8e80
cola2261 <cola2261@proton.me>
2026-06-11 01:38:02 +0500

    dm3q: Move powerhint back to common tree
    Change-Id: I5f5e383b6541e8ca9e643875c4c0cd739d5d667f

--- device.mk
+++ device.mk
@@ -33,10 +33,6 @@ PRODUCT_PACKAGES += \
     SystemUIResDm3q \
     WifiResTargetDm3q
 
-# Power
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
 # Touch features
 PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.samsung
